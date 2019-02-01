3824645558f85d2abac61911ec0a5e063403db9c
Skelman50 <skelman50@gmail.com>
2019-02-01 02:27:03 +0200

    fix

--- calc.ts
+++ calc.ts
@@ -1,17 +1,98 @@
 import * as fs from 'fs';
 
+const readPath = process.argv[2];
+const writePath = './out.txt';
+const result:any = []
 
-fs.readFile('input.txt', function(err, buf) {
- const read = buf.toString()
- const arr = read.split('\n')
- let result = [];
- for(let i = 0; i < arr.length -1; i++) {
-    result.push(eval(arr[i]))
- }
- const write:string = result.join('\n')
- console.log(write)
- fs.writeFile('out.txt', write, function (err) {
-    console.log("Successfully Written to File.");
-});
-});
+function calculate (a:string,b:string,op:string) {
+    if (op === '+') {
+        return +(a) + +(b)
+    }
+    if (op === '-') {
+        return +(a) - +(b)
+    }
+    if (op === '*') {
+        return +(a) * +(b)
+    }
+    if (op === '/') {
+        return +(a) / +(b)
+    }
+}
 
+function validation (string:string):void {
+    const toArray = string.split('\n')
+    for (let i = 0; i < toArray.length; i++) {
+        try{
+        const firstArg = toArray[i].split(' ')[0];
+        let secondArg = toArray[i].split(' ')[2];
+        const operator = toArray[i].split(' ')[1];
+        const validSecondArg = secondArg.split('')
+
+        for (let j = 0; j < validSecondArg.length; j++) {
+            if (validSecondArg[validSecondArg.length-1]  === ',') {
+                validSecondArg.splice(validSecondArg.length-1,1)
+                secondArg = validSecondArg.join('')
+                break
+            }
+        }
+        if (isNaN(+firstArg)) {
+            throw new Error(`First argument on ${i + 1} string isn't a number`)
+        }
+        if (isNaN(+secondArg)) {
+            throw new Error(`Second argument on ${i + 1} string isn't a number`)
+        }
+        if (operator != '+' && toArray[i].split(' ')[1] != '+' &&
+        toArray[i].split(' ')[1] != '+' && toArray[i].split(' ')[1] != '+') {
+            throw new Error(`Can't find operator: ${toArray[i].split(' ')[1]} on ${i + 1} string`) 
+        }
+        result.push(calculate(firstArg, secondArg, operator))
+    }catch (err) {
+        throw new Error(`Don't valid line ${i + 1}`)
+    }
+  }
+}
+
+const readFile = (path: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      if (!path) {
+        reject(new Error('You need provide input path.'));
+      }
+  
+      if (path.indexOf(".txt") === -1) {
+        reject(new Error('Unable to read file shuld be .txt'));
+      }
+  
+      fs.readFile(path, "utf8", (err, data) => {
+        if (err) {
+          reject(err);
+        } 
+        resolve(data);
+      });
+    });
+  }
+
+  const writeFile = (textFile:any): Promise<void>=> {
+      textFile = result.join('\n')
+    return new Promise((resolve, reject) => {
+      fs.writeFile(writePath, textFile, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      })
+    });
+  }
+
+  readFile(readPath)
+  .then(data => validation(data))
+  .then(writeFile)
+  .then(() => console.log(`
+      DONE!
+  `))
+  .catch(err => {
+    console.log(`
+      Error!
+      ${err.message}
+    `);
+  })
+  
--- image_cut.ts
+++ image_cut.ts
@@ -1,7 +1,43 @@
-import * as gm from 'gm';
+import sharp from 'sharp';
 
-gm('./image.jpg')
-.resize('!')
-.write('./out.png', function (err) {
-  if (err) console.log(err);
-});
+const inputImage = process.argv[2]
+const outImage = process.argv[5]
+const imageWidth = process.argv[3]
+const imageHeight = process.argv[4]
+
+function validation (inputImage:string, outImage:string, imageWidth:string, imageHeight:string) {
+  
+    if (!inputImage || !outImage || !imageWidth || !imageHeight) {
+      throw new Error('invalid arguments')
+    }
+    if (isNaN(+imageHeight) || isNaN(+imageWidth)) {
+      throw new Error('Height and width must be a NUMBER')
+    }
+    if (inputImage.indexOf(".png") === -1 && inputImage.indexOf(".jpg") === -1) {
+      throw new Error('Unable to read input image should be .jpg or .png')
+    }
+    if (outImage.indexOf(".png") === -1 && outImage.indexOf(".jpg") === -1) {
+      throw new Error('Unable to read output image should be .jpg or .png')
+    }
+
+}
+const resizeImage = (inputImage:string, outImage:string, imageWidth:string, imageHeight:string) => {
+  try{
+  validation(inputImage, outImage, imageWidth, imageHeight)
+    sharp(inputImage)
+    .resize(+imageWidth, +imageHeight)
+    .toFile(outImage, (err) => {
+      if (err) {
+        throw new Error (err.message)
+      }
+      console.log(`You created the new image!`)
+    })
+  }catch(err) {
+    console.log(`
+    ERROR 
+    ${err.message}`)
+  }
+}
+
+resizeImage(inputImage, outImage, imageWidth, imageHeight)
+
